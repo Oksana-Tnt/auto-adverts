e544ec4e6f28299a3a4e8e64b76afa607a1b76d7
Oksana-Tnt <oksanatantsiura@gmail.com>
2024-02-16 21:45:14 +0100

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom';
-import { CurrentLink } from './Header.styled';
+import React from "react";
+import { Breadcrumb, BreadcrumbItem } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { CurrentLink } from "./Header.styled";
 
 const Header = ({ showModal }) => {
   return (
     <Breadcrumb>
-  <BreadcrumbItem>
-    <CurrentLink as={Link} to='/' >Home</CurrentLink>
-  </BreadcrumbItem>
+      <BreadcrumbItem>
+        <CurrentLink as={Link} to="/">
+          Home
+        </CurrentLink>
+      </BreadcrumbItem>
 
-  <BreadcrumbItem>
-    <CurrentLink as={Link} to='/catalog'>Catalog</CurrentLink>
-  </BreadcrumbItem>
+      <BreadcrumbItem>
+        <CurrentLink as={Link} to="/catalog">
+          Catalog
+        </CurrentLink>
+      </BreadcrumbItem>
 
-  <BreadcrumbItem isCurrentPage>
-    <CurrentLink as={Link} to='/favorite'>Favorite</CurrentLink>
-  </BreadcrumbItem>
-</Breadcrumb>
+      <BreadcrumbItem isCurrentPage>
+        <CurrentLink as={Link} to="/favorite">
+          Favorite
+        </CurrentLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
   );
 };
 
