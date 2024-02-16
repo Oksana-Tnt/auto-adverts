9ceb8c09bd450832f70c22facfa0621b4cbbe0a2
Oksana-Tnt <oksanatantsiura@gmail.com>
2024-02-16 21:53:59 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './components/APP/App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/APP/App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import { GlobalStyle } from "./main.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <BrowserRouter>
+    <BrowserRouter basename="/auto-adverts">
       <Provider store={store}>
-        <GlobalStyle/>
-      <App />     
+        <GlobalStyle />
+        <App />
       </Provider>
-      </BrowserRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
